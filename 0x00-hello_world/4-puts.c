d6eebdaf40f69d3529156e776ab68e2765b00d1a
Ndegwa <ndegwagithua@gmail.com>
2022-09-08 16:34:36 -0700

    puts in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,8 @@
 /**
 *main- print the string in the put function
 *
-* Description: use main function
-*This prigram prints Programming is like building a multilingual puzzle
+*Description: using the main function
+*this prigram prints Programming is like building a multilingual puzzle
 *Return: 0
 */
 {
