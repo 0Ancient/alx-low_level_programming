cedb5b1f69b4a49d2ac853e17d793c5a2614ea48
Ndegwa <ndegwagithua@gmail.com>
2022-09-08 16:25:46 -0700

    puts in C

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+*This prigram prints Programming is like building a multilingual puzzle
+*
+*Return: 0
+*/
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
