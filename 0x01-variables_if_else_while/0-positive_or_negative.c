038cf6e5202863376ad08d07038a7e62861729f2
Ndegwa <ndegwagithua@gmail.com>
2022-09-09 13:44:04 -0700

    Zero positive and negative

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,10 +3,10 @@
 #include <stdio.h>
 
 /**
-* main - print if number if zero, negative or positive
+* main - print if number if positive, zero, or negative
 *
 * Description: use main function
-* program prints if number is zero, negative or positive
+* program prints if number is positive, zero, or negative
 * Return: 0
 */
 int main(void)
@@ -20,7 +20,7 @@ if (n > 0)
 {
 	printf("%i is positive\n", n);
 }
-else if (n==0)
+else if (n == 0)
 {
 	printf("%i is zero\n", n);
 }
