5d759d0783526e0c99de1818b023bf3cba39d3a4
Ndegwa <ndegwagithua@gmail.com>
2022-09-08 17:09:32 -0700

    pring ff

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*main - print string in the put function
+* main - print string in the put function
 *
-*Description: use the main funtion
-*this program prints "Programming is like building a multilingual puzzle
-*Return: 0
+* Description: use the main funtion
+* this program prints "Programming is like building a multilingual puzzle
+* Return: 0
 */
 int main(void)
 {
