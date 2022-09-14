9798dcf17fe4d613a307db3ef329dc103373a144
Ndegwa <ndegwagithua@gmail.com>
2022-09-14 09:16:35 -0700

    small letters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,10 @@
 #include "main.h"
-
 /**
- * print alphabet - function to print abc
+ * print_alphabet - function to print abc
  *
  * Return: 0
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 	char c;
 
