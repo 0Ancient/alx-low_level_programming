7bfe401d3e99778f3f8817d4c2e0b66133008eb9
Ndegwa <ndegwagithua@gmail.com>
2022-09-22 11:37:13 -0700

    touppa

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,31 +2,31 @@
 
 /**
  * string_toupper - changes all lowercase to uppercase
- * @s: string to be changed
+ * @a: string to be changed
  *
  * Return: pointer address to dest
  */
 
-char *string_toupper(char *s)
+char *string_toupper(char *a)
 {
 	int i, j;
 
 	i = 0;
-	while (s[i] != '\0')
+	while (a[i] != '\0')
 	{
 		i++;
 	}
 	for (j = 0 ; j < i ; j++)
 	{
-		if (s[j] > 96 && s[j] < 123)
+		if (a[j] > 96 && a[j] < 123)
 		{
-			s[j] = s[j] - 32;
+			a[j] = a[j] - 32;
 		}
 		else
 		{
-			s[j] = s[j];
+			a[j] = a[j];
 		}
 
 	}
-	return (s);
+	return (a);
 }
